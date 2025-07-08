a572070fc9e95b406c9200f37403a9a641db9ea9
lenalemo8 <yellowscarf8@gmail.com>
2025-07-08 23:44:00 +0300

    API controllers and cleaning for tests

--- test-data/users.ts
+++ test-data/users.ts
@@ -1,5 +1,7 @@
 export const usersList = {
     mainUser: {
+        name: 'TestName',
+        lastName: 'TestLastName',
         email: 'Yellowscarf8@gmail.com',
         password: 'Qwerty123',
     }
